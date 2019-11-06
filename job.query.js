1631e1b160446dc57bac56870b1345e4e6779e2f
Rawat <divyanshu.r46956@gmail.com>
2019-11-06 12:07:18 +0100

    Project completed 👋

--- job.query.js
+++ job.query.js
@@ -1,6 +1,9 @@
 import gql from 'graphql-tag';
 
-
+/*
+     A JavaScript template literal tag that parses GraphQL query strings into the standard GraphQL AST.
+     https://github.com/apollographql/graphql-tag
+*/
 const JOBS_QUERY = gql`
     query Jobs {
        jobs {
